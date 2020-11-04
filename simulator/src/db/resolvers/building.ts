e868d2a86f696303ff541c13c1297e28f1357c23
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-04 13:07:32 +0100

    Building resolver

---
+++ simulator/src/db/resolvers/building.ts
@@ -0,0 +1,54 @@
+import { db } from "../connect";
+import { historyResolver } from "./history"; 
+import { BaseWithHistoryResolver } from "./baseWithHistory";
+
+class BuildingResolver extends BaseWithHistoryResolver {
+    constructor(tableName: string) {
+        super(tableName);
+        this.queries.create = `
+            INSERT INTO ${this.tableName}(battery_buffer, battery_limit, type_id, owner_id, history_id)
+            VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+        this.queries.update = `
+            UPDATE ${this.tableName} SET battery_buffer = $2, battery_limit = $3, type_id = $4, owner_id = $5
+            WHERE id = $1 RETURNING *`;
+        this.queries.updateBatteryBuffer = `
+            UPDATE ${this.tableName} SET battery_buffer = $2 WHERE id = $1 RETURNING *`;
+        this.queries.type = () => {
+            const { buildingTypeResolver } = require('./buildingType');
+            return `
+                SELECT * FROM ${buildingTypeResolver.tableName} WHERE id =
+                (SELECT type_id FROM ${this.tableName} WHERE id = $1);`;
+        }
+    }
+
+    create = async(batteryBuffer: number, batteryLimit: number, type_id: number | string, owner_id: string| number) => {
+        return await db.tx(async t => {
+            const history = await t.one(historyResolver.queries.create);
+            return await t.oneOrNone(this.queries.create, [batteryBuffer, batteryLimit, type_id, owner_id, history.id]);
+        }).catch(err => console.log(err));
+    }
+
+    update = async(id: number | string, batteryBuffer: number, batteryLimit: number, type_id: number | string, owner_id: number | string) => {
+        return await db.tx(async t => {
+            const building = await t.one(this.queries.update, [id, batteryBuffer, batteryLimit, type_id, owner_id]);
+            await t.oneOrNone(historyResolver.queries.update, [building.history_id])
+            return building;
+        }).catch(err => console.log(err));  
+    }
+
+    updateBatteryBuffer = async(id: number | string, batteryBuffer: number) => {
+        return await db.tx(async t => {
+            const building = await t.one(this.queries.updateBatteryBuffer, [id, batteryBuffer]);
+            await t.oneOrNone(historyResolver.queries.update, [building.history_id])
+            return building;
+        }).catch(err => console.log(err));
+    }
+
+    type = async (id: number | string) => {
+        return db
+            .oneOrNone(this.queries.type, [id])
+            .catch(err => console.log(err));
+    }
+}
+
+export const buildingResolver = new BuildingResolver('buildings');
