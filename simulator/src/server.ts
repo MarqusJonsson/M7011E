4bc1331aaf1b3477a2ec36aa47de1194b867b78d
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-04 21:49:54 +0100

    Graphql endpoint

--- simulator/src/server.ts
+++ simulator/src/server.ts
@@ -3,12 +3,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { cors } from './middleware/cors';
 import { errorHandler } from './middleware/errorHandler';
+// GraphQL
+import { GraphQLSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
+import { RootQuery } from './schemas/root/queries';
+import { RootMutation } from './schemas/root/mutations';
+const schema = new GraphQLSchema({ query: RootQuery, mutation: RootMutation });
 // Setup server
 const PORT = process.env.SIMULATOR_SERVER_PORT;
 const app = express();
 // Setup middleware
 app.use(bodyParser.json());
 app.use(cors(['http://localhost:3002', 'http://localhost:4200']));
+app.use('/graphiql', graphqlHTTP({ schema: schema, graphiql: true }));
 app.use(errorHandler);
 // Start server
 app.listen(PORT, () => console.log(`Simulator server listening on port ${PORT}`));
