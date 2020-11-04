1cf21e2273c5d56bd3a477e4c73e0554aef472b8
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-04 12:51:36 +0100

    Create database tables

---
+++ simulator/src/db/create.ts
@@ -0,0 +1,103 @@
+import { db } from './connect';
+
+const createTables = `
+	CREATE TABLE history (
+		id		 SERIAL PRIMARY KEY,
+		created_at TIMESTAMPTZ NOT NULL,
+		updated_at TIMESTAMPTZ NOT NULL
+	);
+	CREATE TABLE roles (
+		id		 SERIAL PRIMARY KEY,
+		name	   VARCHAR (255) NOT NULL UNIQUE,
+		history_id INT		   NOT NULL,
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE users (
+		id			  SERIAL PRIMARY KEY,
+		email		   VARCHAR (255) NOT NULL UNIQUE,
+		currency		REAL		  NOT NULL,
+		roles_id		INT		   NOT NULL,
+		history_id	  INT		   NOT NULL,
+		FOREIGN KEY (roles_id)
+			REFERENCES roles (id),
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE transactions (
+		id		  SERIAL PRIMARY KEY,
+		amount	  REAL NOT NULL,
+		sender_id   INT  NOT NULL,
+		receiver_id INT  NOT NULL,
+		history_id  INT  NOT NULL,
+		FOREIGN KEY (sender_id)
+			REFERENCES users (id),
+		FOREIGN KEY (receiver_id)
+			REFERENCES users (id),
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE building_type (
+		id		 SERIAL PRIMARY KEY,
+		name	   VARCHAR (255) NOT NULL UNIQUE,
+		history_id INT		   NOT NULL,
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE buildings (
+		id			 SERIAL PRIMARY KEY,
+		battery_buffer REAL NOT NULL,
+		battery_limit  REAL NOT NULL,
+		type_id	  INT  NOT NULL,
+		owner_id	   INT  NOT NULL,
+		history_id	 INT  NOT NULL,
+		FOREIGN KEY (type_id)
+			REFERENCES building_type (id),
+		FOREIGN KEY (owner_id)
+			REFERENCES users (id),
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE generator_tpe (
+		id		 SERIAL PRIMARY KEY,
+		name	   VARCHAR (255) NOT NULL UNIQUE,
+		history_id INT		   NOT NULL,
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE generators (
+		id		   SERIAL PRIMARY KEY,
+		base_output  REAL	NOT NULL,
+		is_broken	BOOLEAN NOT NULL,
+		type_id	INT	 NOT NULL,
+		buildings_id INT	 NOT NULL,
+		history_id   INT	 NOT NULL,
+		FOREIGN KEY (type_id)
+			REFERENCES generator_type(id),
+		FOREIGN KEY (buildings_id)
+			REFERENCES buildings(id),
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE generator_history_type (
+		id		 SERIAL PRIMARY KEY,
+		name	   VARCHAR (255) NOT NULL UNIQUE,
+		history_id INT		   NOT NULL,
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+	);
+	CREATE TABLE generator_history (
+		id						  SERIAL PRIMARY KEY,
+		amount					  REAL NOT NULL,
+		type_id INT  NOT NULL,
+		generators_id			   INT  NOT NULL,
+		history_id				  INT  NOT NULL,
+		FOREIGN KEY (type_id)
+			REFERENCES generator_history_type (id),
+		FOREIGN KEY (generators_id)
+			REFERENCES generators (id),
+		FOREIGN KEY (history_id)
+			REFERENCES history (id)
+);`;
+
+export const create = () => db.none(createTables);
