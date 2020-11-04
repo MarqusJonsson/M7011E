ac4a8000d315a7a1f1ec904d09e899c71566068f
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-04 21:47:52 +0100

    Root schemas

---
+++ simulator/src/schemas/root/mutations.ts
@@ -0,0 +1,39 @@
+import { GraphQLObjectType } from 'graphql';
+import { createHistory, removeHistory, updateHistory } from '../history/mutations';
+import { createUserType, removeUserType, updateUserType } from '../userType/mutations';
+import { createUser, removeUser, updateUser, updateUserCurrency } from '../user/mutations';
+import { createBuilding, updateBuilding, removeBuilding, updateBatteryBuffer } from '../building/mutations';
+import { createBuildingType, removeBuildingType, updateBuildingType } from '../buildingType/mutations';
+import { createGenerator, updateGenerator, removeGenerator } from '../generator/mutations';
+import { createGeneratorType, removeGeneratorType, updateGeneratorType } from '../generatorType/mutations';
+
+const RootMutation = new GraphQLObjectType({
+	name: 'RootMutation',
+	fields: {
+		createHistory: createHistory,
+		removeHistory: removeHistory,
+		updateHistory: updateHistory,
+		createUserType: createUserType,
+		removeUserType: removeUserType,
+		updateUserType: updateUserType,
+		createUser: createUser,
+		removeUser: removeUser,
+		updateUser: updateUser,
+		updateUserCurrency: updateUserCurrency,
+		createBuildingType: createBuildingType,
+		removeBuildingType: removeBuildingType,
+		updateBuildingType: updateBuildingType,
+		createBuilding: createBuilding,
+		removeBuilding: removeBuilding,
+		updateBuilding: updateBuilding,
+        updateBuildingBatteryBuffer: updateBatteryBuffer,
+        createGeneratorType: createGeneratorType,
+		removeGeneratorType: removeGeneratorType,
+		updateGeneratorType: updateGeneratorType,
+		createGenerator: createGenerator,
+		removeGenerator: removeGenerator,
+		updateGenerator: updateGenerator,
+	}
+});
+
+export { RootMutation };
---
+++ simulator/src/schemas/root/queries.ts
@@ -0,0 +1,30 @@
+import { GraphQLObjectType } from 'graphql';
+import { histories, history } from '../history/queries';
+import { userTypes, userType } from '../userType/queries';
+import { users, user } from '../user/queries';
+import { buildings, building } from '../building/queries';
+import { buildingType, buildingTypes } from '../buildingType/queries';
+import { generators, generator } from '../generator/queries';
+import { generatorType, generatorTypes } from '../generatorType/queries';
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQuery',
+	fields: {
+		histories: histories,
+		history: history,
+		userTypes: userTypes,
+		userType: userType,
+		users: users,
+		user: user,
+		buildingTypes: buildingTypes,
+		buildingType: buildingType,
+		buildings: buildings,
+		building: building,
+		generatorTypes: generatorTypes,
+		generatorType: generatorType,
+		generators: generators,
+		generator: generator
+	}
+});
+
+export { RootQuery };
