1e6166c28083ade7b86cc7aa088b9a485637ed95
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-04 21:46:24 +0100

    History schemas

---
+++ simulator/src/schemas/history/queries.ts
@@ -0,0 +1,25 @@
+import { typeName, HistoryType } from './types';
+import { historyResolver } from '../../db/resolvers/history';
+import { GraphQLID, GraphQLList } from 'graphql';
+
+const histories = {
+	type: new GraphQLList(HistoryType),
+	description: `Returns all histories.`,
+	resolve() {
+		return historyResolver.all();
+	}
+};
+
+const history = {
+	type: HistoryType,
+	description: `Returns a ${typeName} with matching id.`,
+	args: { id: { type: GraphQLID } },
+	resolve(parent: any, args: any) {
+		return historyResolver.one(args.id);
+	}
+};
+
+export {
+	histories,
+	history
+};
