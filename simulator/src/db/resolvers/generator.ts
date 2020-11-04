70df2adc8d6ac2cd88cc200b9cfb203210613ce3
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-04 13:41:54 +0100

    Generator resolver

---
+++ simulator/src/db/resolvers/generator.ts
@@ -0,0 +1,36 @@
+import { db } from "../connect";
+import { historyResolver } from "./history"; 
+import { BaseWithHistoryResolver } from "./baseWithHistory";
+class GeneratorResolver extends BaseWithHistoryResolver{
+	constructor(tableName: string){
+		super(tableName);
+		this.queries.create = `
+			INSERT INTO ${this.tableName}(base_output, is_broken, generator_types_id, buildings_id, history_id) RETURNING *`;
+		this.queries.update = `
+			UPDATE ${this.tableName} SET base_output = $2, is_broken = $3, generator_types_id = $4, buildings_id = $5
+			WHERE id = $1 RETURNING *`;
+		this.queries.generator_type = () => {
+			const { generatorTypeResolver } = require('./generatorType');
+			return `
+				SELECT * FROM ${generatorTypeResolver.tableName} WHERE id =
+				(SELECT generator_types_id FROM ${this.tableName} WHERE id = $1);`;
+		}
+	}
+
+	create = async(baseOutput: number, isBroken: boolean, generator_types_id: string|number, buildings_id: string|number) =>{
+		return await db.tx(async t => {
+			const history = await t.one(historyResolver.queries.create);
+			return await t.oneOrNone(this.queries.create, [baseOutput, isBroken, generator_types_id, buildings_id, history.id]);
+		}).catch(err => console.log(err));
+	}
+
+	update = async (id: string|number, baseOutput: number, isBroken: boolean, generator_types_id: string|number, buildings_id: string|number) => {
+		return await db.tx(async t => {
+			const generator = await t.one(this.queries.update, [id, baseOutput, isBroken, generator_types_id, buildings_id]);
+			await t.oneOrNone(historyResolver.queries.update, [generator.history_id])
+			return generator;
+		}).catch(err => console.log(err));
+	}
+}
+
+export const generatorResolver = new GeneratorResolver('generators');
