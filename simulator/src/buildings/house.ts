d625911c9fe74c7204ebfb59da7c9541eaa5aa42
osksun <37369260+osksun@users.noreply.github.com>
2020-11-04 13:03:11 +0100

    Add generateElectricity

--- simulator/src/buildings/house.ts
+++ simulator/src/buildings/house.ts
@@ -17,3 +17,44 @@ export class House extends BaseBuilding {
 		this._powerPlantParent = powerPlantParent;
 		this._prosumer = prosumer;
 	}
+
+	public generateElectricity(deltaTimeS: number) {
+		const totalProduction = this.production * deltaTimeS;
+		const productionToBattery: number = totalProduction * this.batteryToPowerPlantRatio;
+		const productionToPowerPlant: number = totalProduction * (1 - this.batteryToPowerPlantRatio);
+		let electricitySold = 0;
+		const hBattery: Battery = this.battery;
+		const pBattery: Battery = this.powerPlantParent.battery;
+		if (hBattery.buffer + productionToBattery <= hBattery.capacity) {
+			hBattery.buffer += productionToBattery;
+			// Send power to power plant
+			if (pBattery.buffer + productionToPowerPlant >= pBattery.capacity){
+				electricitySold = pBattery.capacity - pBattery.buffer;
+				pBattery.buffer = pBattery.capacity;
+				// Since power plant battery was filled, send rest to house battery
+				hBattery.buffer = Math.min(hBattery.capacity, hBattery.buffer + productionToPowerPlant - electricitySold);
+			}
+			else {
+				electricitySold = productionToPowerPlant;
+				pBattery.buffer += productionToPowerPlant;
+			}
+		}
+		else {
+			const excessElectricity: number = hBattery.buffer + productionToBattery - this.battery.capacity;
+			hBattery.buffer = hBattery.capacity;
+			// productionToPowerPlant + excessElectricity to power plant
+			if (pBattery.buffer + excessElectricity >= pBattery.capacity){
+				electricitySold = pBattery.buffer - pBattery.capacity;
+				pBattery.buffer = pBattery.capacity;
+				// Since power plant battery was filled, send rest to house battery
+				hBattery.buffer = Math.min(hBattery.capacity, hBattery.buffer + excessElectricity - electricitySold);
+			}
+			else {
+				electricitySold = excessElectricity;
+				pBattery.buffer += excessElectricity;   
+			}
+		}
+		const payment = this.powerPlantParent.calculateElectricityPrice(electricitySold);
+		this.prosumer.currency += payment;
+		this.powerPlantParent.manager.currency -= payment;
+	}
