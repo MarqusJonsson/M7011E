38e310a412a0b46f942eae7d78111f0cece17d45
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-05 16:08:11 +0100

    Create transaction resolver

--- simulator/src/db/resolvers/transaction.ts
+++ simulator/src/db/resolvers/transaction.ts
@@ -1,6 +1,7 @@
 import { db } from '../connection';
 import { historyResolver } from './history';
 import { BaseWithHistoryResolver } from './baseWithHistory';
+import { userResolver } from './user';
 import { ITask } from 'pg-promise';
 
 class TransactionResolver extends BaseWithHistoryResolver {
@@ -13,8 +14,10 @@ class TransactionResolver extends BaseWithHistoryResolver {
 
 	create = async (amount: number, senderId: number | string, receiverId: number | string): Promise<any> => {
 		return await db.tx(async (t: ITask<{}>) => {
+			await t.one(userResolver.queries.updateCurrency, [senderId, -amount]);
+			await t.one(userResolver.queries.updateCurrency, [receiverId, amount]);
 			const history = await t.one(historyResolver.queries.create);
-			return await t.oneOrNone(this.queries.create, [amount, senderId, receiverId, history.id]);
+			return await t.one(this.queries.create, [amount, senderId, receiverId, history.id]);
 		}).catch(err => console.log(err));
 	}
 }
