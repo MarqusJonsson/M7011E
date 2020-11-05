25d982a2516e6e92c7a2241545c603ff42c27d44
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-05 16:22:36 +0100

    Reorder queries

--- simulator/src/schemas/root/queries.ts
+++ simulator/src/schemas/root/queries.ts
@@ -3,9 +3,9 @@ import { histories, history } from '../history/queries';
 import { userTypes, userType } from '../userType/queries';
 import { users, user } from '../user/queries';
 import { buildings, building } from '../building/queries';
-import { buildingType, buildingTypes } from '../buildingType/queries';
+import { buildingTypes, buildingType } from '../buildingType/queries';
 import { generators, generator } from '../generator/queries';
-import { generatorType, generatorTypes } from '../generatorType/queries';
+import { generatorTypes, generatorType } from '../generatorType/queries';
 import { transactions, transaction } from '../transaction/queries';
 
 const RootQuery = new GraphQLObjectType({
