68a75355a7f7fe2a4e4bdbe8e85997c75c5daed0
osksun <37369260+osksun@users.noreply.github.com>
2020-11-10 17:03:40 +0100

    Remove circular dependency

--- simulator/src/buildings/coalPowerPlant.ts
+++ simulator/src/buildings/coalPowerPlant.ts
@@ -2,11 +2,9 @@ import { Battery } from './components/battery';
 import { GeoData } from './components/geoData';
 import { BaseGenerator } from '../generators/baseGenerator';
 import { BasePowerPlant } from './basePowerPlant';
-import { Manager } from '../users/manager';
-import { Environment } from '../environment';
 
 export class CoalPowerPlant extends BasePowerPlant{
-	constructor(battery: Battery, geoData: GeoData, generators: BaseGenerator[], consumption: number, manager: Manager, environment: Environment){
-		super(CoalPowerPlant.name, battery, geoData, generators, consumption, manager, environment);
+	constructor(battery: Battery, geoData: GeoData, generators: BaseGenerator[], consumption: number){
+		super(CoalPowerPlant.name, battery, geoData, generators, consumption);
 	}
 }
--- simulator/src/generators/coalGenerator.ts
+++ simulator/src/generators/coalGenerator.ts
@@ -1,14 +1,14 @@
 import { BaseGenerator } from './baseGenerator';
-import { BaseBuilding } from '../buildings/baseBuilding';
+import { Environment } from '../environment';
+import { GeoData } from '../buildings/components/geoData';
 
 export class CoalGenerator extends BaseGenerator {
-    constructor(baseOutput: number = 0, isBroken: boolean = false, parentBuilding?: BaseBuilding, pollution: number = 0) {
-        super(baseOutput, isBroken, parentBuilding, pollution);
+    constructor(baseOutput: number = 0, isBroken: boolean = false, pollution: number = 0) {
+        super(baseOutput, isBroken, pollution);
     }
 
-    public calculateOutput(): number {
-        if (!this.parentBuilding) throw new Error('Can not calculate the output of a generator without a parentBuilding');
-		else if (this.isBroken) this.output = 0;
+    public calculateOutput(environment: Environment, geoData: GeoData): number {
+		if (this.isBroken) this.output = 0;
 		else this.output = this.baseOutput;
         return this.output;
     }
