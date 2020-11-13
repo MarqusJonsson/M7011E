3ea38239b99465e9dca8832292126a5711e21ef6
osksun <37369260+osksun@users.noreply.github.com>
2020-11-13 20:49:05 +0100

    Add wrapper for bilinearinterpolation

--- simulator/src/math/interpolation.ts
+++ simulator/src/math/interpolation.ts
@@ -1,3 +1,5 @@
+import { GeoData } from "../buildings/components/geoData";
+
 export function calc1DInterpolationConstants(x1: number, x2: number, x: number): number[] {
 	const dist_x1_x2 = x2 - x1;
 	const dist_x_x2 = x2 - x;
@@ -14,19 +16,19 @@ export function apply1DInterpolation(c: number[], h1:number, h2: number) {
 }
 
 export function bilinearInterpolation(q11_x: number, q11_y: number, h11: number, q21_x: number, q21_y: number, h21: number, q12_x: number, q12_y: number, h12: number, q22_x: number, q22_y: number, h22: number, p_x: number, p_y: number) {
-
 	const r1c_x = calc1DInterpolationConstants(q11_x, q21_x, p_x);
 	const r1h_x = apply1DInterpolation(r1c_x, h11, h21);
 	const r1h_y = r1h_x;
 	const r2c_x = calc1DInterpolationConstants(q12_x, q22_x, p_x);
 	const r2h_x = apply1DInterpolation(r2c_x, h12, h22);
 	const r2h_y = r2h_x;
-
-
 	const p_c_y = calc1DInterpolationConstants(q11_y,q12_y,p_y);
-
 	const hp = apply1DInterpolation(p_c_y, r1h_y, r2h_y);
 	return hp;
+}
+
+export function geoDatabilinearInterpolation(q11: GeoData, q11Value: number, q21: GeoData, q21Value: number, q12: GeoData, q12Value: number, q22:GeoData, q22Value: number, p: GeoData){
+	return bilinearInterpolation(q11.longitude, q11.latitude, q11Value, q21.longitude, q21.latitude, q21Value, q12.longitude, q12.latitude, q12Value, q22.longitude, q22.latitude, q22Value, p.longitude, p.latitude);
 }
 
 export function bilinearTest(){
