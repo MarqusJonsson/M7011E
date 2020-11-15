f95c7d676010d63d2735119ff8855f08a1434a9f
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-15 20:43:02 +0100

    Update unit tests to match new design choices

--- simulator/test/buildings/baseBuilding.test.ts
+++ simulator/test/buildings/baseBuilding.test.ts
@@ -8,27 +8,19 @@ import { Manager } from '../../src/users/manager';
 import { Environment } from '../../src/environment';
 
 class TestObject{
-	environment!: Environment;
-	pBattery!: Battery;
-	pGeoData!: GeoData;
-	manager!: Manager;
-	coalPowerPlant!: CoalPowerPlant;
 	hBattery!: Battery;
-	hGeoData!: GeoData;
-	hCoalGenerator!: CoalGenerator;
 	house!: House;
 
 	public defaultValues(): void {
-		this.environment = new Environment(0);
-		this.pBattery = new Battery(2000, 0);
-		this.pGeoData = new GeoData(100, 10, 10);
-		this.coalPowerPlant = new CoalPowerPlant(this.pBattery, this.pGeoData, [], 0);
-		this.manager = new Manager(0, [this.coalPowerPlant]);
+		const pBattery = new Battery(2000, 0);
+		const pGeoData = new GeoData(100, 10, 10);
+		const coalPowerPlant = new CoalPowerPlant(pBattery, pGeoData, []);
+		const manager = new Manager(0, coalPowerPlant);
 		
 		this.hBattery = new Battery(100, 0);
-		this.hGeoData = new GeoData(10, 10, 10);
-		this.hCoalGenerator = new CoalGenerator(100, false, 0);
-		this.house = new House(this.hBattery, this.hGeoData, [this.hCoalGenerator], 40, 0.1, [this.manager]);
+		const hGeoData = new GeoData(10, 10, 10);
+		const hCoalGenerator = new CoalGenerator(100, false, 0);
+		this.house = new House(this.hBattery, hGeoData, [hCoalGenerator], 0.1);
 	}
 }
 
@@ -38,27 +30,31 @@ describe('buildings/baseBuilding.ts', function() {
 		beforeEach('Setup default values for test object.', function() {testObject.defaultValues()});
 		it('Consume insufficient energy and blackout.', function() {
 			testObject.house.battery.buffer = 20;
-			testObject.house.consumeElectricity(1, testObject.environment);
+			testObject.house.consumption = 40;
+			testObject.house.consumeElectricity();
 			expect(testObject.house.hasBlackout).to.equal(true);
 			expect(testObject.house.battery.buffer).to.equal(0);
 		});
 		it('Consume energy up to demand.', function() {
 			testObject.hBattery.buffer = 50;
-			testObject.house.consumeElectricity(1, testObject.environment);
+			testObject.house.consumption = 40;
+			testObject.house.consumeElectricity();
 			expect(testObject.house.hasBlackout).to.equal(false);
 			expect(testObject.house.battery.buffer).to.equal(10);
 		});
 		it('Consume energy up to demand and return from blackout.', function() {
 			testObject.house.hasBlackout = true;
 			testObject.hBattery.buffer = 50;
-			testObject.house.consumeElectricity(1, testObject.environment);
+			testObject.house.consumption = 40;
+			testObject.house.consumeElectricity();
 			expect(testObject.house.hasBlackout).to.equal(false);
 			expect(testObject.house.battery.buffer).to.equal(10);
 		});
 		it('Consume nothing due to not reaching demand and do not return from blackout.', function() {
 			testObject.house.hasBlackout = true;
 			testObject.hBattery.buffer = 20;
-			testObject.house.consumeElectricity(1, testObject.environment);
+			testObject.house.consumption = 30;
+			testObject.house.consumeElectricity();
 			expect(testObject.house.hasBlackout).to.equal(true);
 			expect(testObject.house.battery.buffer).to.equal(20);
 		});
--- simulator/test/buildings/basePowerPlant.test.ts
+++ simulator/test/buildings/basePowerPlant.test.ts
@@ -7,19 +7,15 @@ import { expect } from 'chai';
 
 class TestObject{
 	environment!: Environment;
-	pBattery!: Battery;
-	pGeoData!: GeoData;
-	pConsumption!: number;
 	coalPowerPlant!: CoalPowerPlant;
 	pCoalGenerator!: CoalGenerator;
 
 	public defaultValues(): void {
 		this.environment = new Environment(0);
-		this.pBattery = new Battery(2000, 0);
-		this.pGeoData = new GeoData(100, 10, 10);
+		const pBattery = new Battery(2000, 0);
+		const pGeoData = new GeoData(100, 10, 10);
 		this.pCoalGenerator = new CoalGenerator(100, false, 0);
-		this.pConsumption = 100;
-		this.coalPowerPlant = new CoalPowerPlant(this.pBattery, this.pGeoData, [this.pCoalGenerator], this.pConsumption);
+		this.coalPowerPlant = new CoalPowerPlant(pBattery, pGeoData, [this.pCoalGenerator]);
 	}
 }
 
@@ -29,20 +25,20 @@ describe('buildings/basePowerPlant.ts', function() {
 		beforeEach('Setup default values for test object.', function() {testObject.defaultValues()});
 		it('Generate electricity and add it to battery.', function() {
 			testObject.coalPowerPlant.calculateProduction(1, testObject.environment);
-			testObject.coalPowerPlant.generateElectricity(1);
+			testObject.coalPowerPlant.generateElectricity();
 			expect(testObject.coalPowerPlant.battery.buffer).to.equal(100);
 		});
 		it('Generate electricity, reach upper production limit and stop production.', function() {
 			testObject.pCoalGenerator.baseOutput = 1201;
 			testObject.coalPowerPlant.calculateProduction(1, testObject.environment);
-			testObject.coalPowerPlant.generateElectricity(1);
+			testObject.coalPowerPlant.generateElectricity();
 			expect(testObject.coalPowerPlant.battery.buffer).to.equal(1201);
 			expect(testObject.coalPowerPlant.productionFlag).to.equal(false);
 		});
 		it('Generate electricity, reach upper production limit and stop production.', function() {
 			testObject.pCoalGenerator.baseOutput = 1201;
 			testObject.coalPowerPlant.calculateProduction(1, testObject.environment);
-			testObject.coalPowerPlant.generateElectricity(1);
+			testObject.coalPowerPlant.generateElectricity();
 			expect(testObject.coalPowerPlant.battery.buffer).to.equal(1201);
 			expect(testObject.coalPowerPlant.productionFlag).to.equal(false);
 		});
@@ -50,14 +46,14 @@ describe('buildings/basePowerPlant.ts', function() {
 			testObject.coalPowerPlant.productionFlag = false;
 			testObject.pCoalGenerator.baseOutput = 400;
 			testObject.coalPowerPlant.calculateProduction(1, testObject.environment);
-			testObject.coalPowerPlant.generateElectricity(1);
+			testObject.coalPowerPlant.generateElectricity();
 			expect(testObject.coalPowerPlant.battery.buffer).to.equal(0);
 			expect(testObject.coalPowerPlant.productionFlag).to.equal(true);
 		});
 		it('Generate electricity, go over battery capacity and stop production.', function() {
 			testObject.pCoalGenerator.baseOutput = 2001;
 			testObject.coalPowerPlant.calculateProduction(1, testObject.environment);
-			testObject.coalPowerPlant.generateElectricity(1);
+			testObject.coalPowerPlant.generateElectricity();
 			expect(testObject.coalPowerPlant.battery.buffer).to.equal(2000);
 			expect(testObject.coalPowerPlant.productionFlag).to.equal(false);
 		});
