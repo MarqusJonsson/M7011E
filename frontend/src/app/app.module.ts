4a1df5a5724efcec25fa0bbc22a3926c2226e3d5
osksun <37369260+osksun@users.noreply.github.com>
2020-11-18 13:54:31 +0100

    Add login and alert to app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/containers/login/login.component';
+import { AlertComponent } from './alert/containers/alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
