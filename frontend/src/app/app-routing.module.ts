ec0c92d558cf4c11f7185d49409a4ee37553efd2
osksun <37369260+osksun@users.noreply.github.com>
2020-11-18 15:20:47 +0100

    Add routing to prosumer page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/containers/login/login.component';
+import { ProsumerPageComponent } from './user_pages/prosumer/containers/prosumer-page/prosumer-page.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path:  'login', component: LoginComponent },
+  {path:  'prosumerPage', component: ProsumerPageComponent },
 
 ];
 
