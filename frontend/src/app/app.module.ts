6356500d76b96c00df505d30e391dbc51b2b88cf
osksun <37369260+osksun@users.noreply.github.com>
2020-11-18 16:32:17 +0100

    Add account settings component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/containers/login/login.component';
 import { AlertComponent } from './alert/containers/alert/alert.component';
-import { ProsumerPageComponent } from './user_pages/prosumer/containers/prosumer-page/prosumer-page.component';
+import { ProsumerPageComponent } from './users/prosumer/containers/prosumer-page/prosumer-page.component';
+import { AccountSettingsComponent } from './users/shared/containers/account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AlertComponent,
-    ProsumerPageComponent
+    ProsumerPageComponent,
+    AccountSettingsComponent
   ],
   imports: [
     BrowserModule,
