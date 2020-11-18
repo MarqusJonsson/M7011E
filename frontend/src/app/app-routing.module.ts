077e447aa5f2668d887ba6d219b5ece30fb3bbaa
osksun <37369260+osksun@users.noreply.github.com>
2020-11-18 21:08:22 +0100

    Fix imports

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/containers/login/login.component';
-import { ProsumerPageComponent } from './user_pages/prosumer/containers/prosumer-page/prosumer-page.component';
+import { ProsumerPageComponent } from './users/prosumer/containers/prosumer-page/prosumer-page.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
