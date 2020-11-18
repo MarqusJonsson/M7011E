2592d4bed1f477466031716c17ac09dd50e28ab5
osksun <37369260+osksun@users.noreply.github.com>
2020-11-18 15:45:00 +0100

    Add alert service to login component

--- frontend/src/app/login/containers/login/login.component.ts
+++ frontend/src/app/login/containers/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+;
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from "../../../alert/services/alert.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private alertService: AlertService,private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    //Redirect from login page if already logged in
+    this.form = this.formBuilder.group({
+    email: ['',Validators.required],
+    password: ['',Validators.required]
+    });
   }
 
+  login() {
+    this.alertService.clear();
+    if (this.form.invalid) {
+      this.alertService.error("Invalid form credentials");
+    }
+    else{
+      this.router.navigateByUrl('prosumerPage');
+    }
+  }
 }
