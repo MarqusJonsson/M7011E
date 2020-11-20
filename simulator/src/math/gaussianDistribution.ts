053b53a493558b95e0fa3c7aab288b5ab4df11a0
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-20 01:39:24 +0100

    Spaces to tabs

--- simulator/src/math/gaussianDistribution.ts
+++ simulator/src/math/gaussianDistribution.ts
@@ -1,13 +1,13 @@
 import { Gaussian } from 'ts-gaussian';
 
 export class GaussianDistribution {
-    private _distribution: Gaussian;
-    
-    constructor(mean: number, variance: number) {
-        this._distribution = new Gaussian(mean, variance);
-    }
+	private _distribution: Gaussian;
+	
+	constructor(mean: number, variance: number) {
+		this._distribution = new Gaussian(mean, variance);
+	}
 
-    public sample() {
-        return this._distribution.ppf(Math.random());
-    }
+	public sample() {
+		return this._distribution.ppf(Math.random());
+	}
 }
