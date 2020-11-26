31160f0ab60fd87312980a72461158b0c71bf66e
osksun <37369260+osksun@users.noreply.github.com>
2020-11-26 16:59:33 +0100

    Add route to settings page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/containers/login/login.component';
 import { ProsumerPageComponent } from './users/prosumer/containers/prosumer-page/prosumer-page.component';
+import { AccountSettingsComponent } from './users/shared/containers/account-settings/account-settings.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path:  'login', component: LoginComponent },
   {path:  'prosumerPage', component: ProsumerPageComponent },
+  {path: 'settings' , component: AccountSettingsComponent}
 
 ];
 
