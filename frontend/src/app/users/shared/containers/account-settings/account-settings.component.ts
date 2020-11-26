29eec31544640ccfe3393163276c89286e024f85
osksun <37369260+osksun@users.noreply.github.com>
2020-11-26 18:13:46 +0100

    Remove unused code

--- frontend/src/app/users/shared/containers/account-settings/account-settings.component.ts
+++ frontend/src/app/users/shared/containers/account-settings/account-settings.component.ts
@@ -1,20 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../../../auth/services/auth.service"
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
-  constructor(private authService: AuthService) { }
+  constructor() { }
   ngOnInit(): void {
   }
-  public checkLoginStatus(): string{
-    if(this.authService.authCheck()){
-      return "Logged in as " + this.authService.getEmail();
-    }
-      return "Currently not logged in";
-  }
+
   
 
 }
