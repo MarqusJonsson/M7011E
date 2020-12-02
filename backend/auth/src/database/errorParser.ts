330786d70e4d6eea833bdeb05fbefdbd2989dd0e
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-02 18:26:07 +0100

    Error parser for database communication

---
+++ backend/auth/src/database/errorParser.ts
@@ -0,0 +1,47 @@
+import pgPromise from 'pg-promise';
+import { ResponseError } from '../utils/error';
+import { StatusCode } from '../utils/statusCode';
+
+// PostgreSQL error codes appendix at https://www.postgresql.org/docs/13/errcodes-appendix.html
+enum PostgreSQLErrorCodes {
+	UNIQUE_VIOLATION = '23505'
+}
+
+enum PostgreSQLConstraints {
+	USERS_EMAIL_KEY = 'users_email_key'
+}
+
+export function toResponseError(error: any): ResponseError {
+	if (error instanceof ResponseError) {
+		return error;
+	} else if (error instanceof pgPromise.errors.QueryResultError) {
+		return parseQueryResultError(error);
+	} else {
+		error = parsePostgreSQLError(error)
+		return error;
+	}
+}
+
+function parseQueryResultError(error: pgPromise.errors.QueryResultError) {
+	switch (error.code) {
+		case pgPromise.errors.queryResultErrorCode.noData:
+			switch (error.result.command) {
+				case 'DELETE':
+					return new ResponseError('Requested resouce to be deleted was not found', StatusCode.NOT_FOUND);
+			}
+			return new ResponseError(error.message, StatusCode.NOT_FOUND);
+	}
+	return new ResponseError();
+}
+
+function parsePostgreSQLError(error: any) {
+	switch (error.code) {
+		case PostgreSQLErrorCodes.UNIQUE_VIOLATION:
+			switch (error.constraint) {
+				case PostgreSQLConstraints.USERS_EMAIL_KEY:
+					return new ResponseError('Email is already in use', StatusCode.CONFLICT);
+			}
+	}
+	console.error(error);
+	return new ResponseError();
+}
