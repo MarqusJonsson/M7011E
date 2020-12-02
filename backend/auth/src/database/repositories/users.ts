3015a61a0fdb16facc0856a636485214aa68f6d2
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-02 18:27:47 +0100

    User repository

---
+++ backend/auth/src/database/repositories/users.ts
@@ -0,0 +1,77 @@
+import pgPromise from 'pg-promise';
+import { ResponseError } from '../../utils/error';
+import { StatusCode } from '../../utils/statusCode';
+import { toResponseError } from '../errorParser';
+import { User } from '../models/user';
+import { BaseWithHistoryRepository } from './baseWithHistory';
+import { HistoryRepository } from './history';
+
+const tableName = 'users';
+export class UsersRepository extends BaseWithHistoryRepository<User> {
+	constructor(database: pgPromise.IDatabase<any>, historyRepository: HistoryRepository) {
+		super(database, tableName, historyRepository);
+	}
+
+	public createTable(): Promise<null> {
+		return this.database.none(`
+			CREATE TABLE IF NOT EXISTS ${this.tableName}(
+				id SERIAL PRIMARY KEY,
+				email VARCHAR (255)	NOT NULL UNIQUE,
+				password VARCHAR (60) NOT NULL,
+				histories_id INT NOT NULL REFERENCES histories (id)
+			)`
+		);
+	}
+
+	public insert(email: string, password: string, t?: pgPromise.ITask<any>): Promise<number> {
+		return new Promise((resolve, reject) => {
+			if (t !== undefined) {
+				
+			}
+			const db = t || this.database;
+			return db.txIf((t_: pgPromise.ITask<any>) => {
+				return this.historyRepository.insert(t_).then((insertedHistoryId) => {
+					return t_.one(`INSERT INTO ${this.tableName}(email, password, histories_Id) VALUES ($1, $2, $3) RETURNING id`, [email, password, insertedHistoryId]).then((insertedUser) => {
+						resolve(insertedUser.id);
+					}).catch((error) => { reject(toResponseError(error)); });
+				}).catch((error) => { reject(toResponseError(error)); });
+			});
+		});
+	}
+	
+	public findByEmail(email: string, t?: pgPromise.ITask<any>): Promise<User> {
+		return new Promise((resolve, reject) => {
+			const db = t || this.database;
+			return db.taskIf((t_: pgPromise.ITask<any>) => {
+				return t_.one(`SELECT id, email, histories_id FROM ${this.tableName} WHERE email = $1`, email).then((user) => {
+					return this.historyRepository.find(user.histories_id, t_).then((history) => {
+						user.history = history;
+						resolve(user);
+					}).catch((error) => { reject(toResponseError(error)); });
+				}).catch((error) => { reject(toResponseError(error)); });
+			});
+		});
+	}
+
+	public findPasswordByEmail(email: string, t?: pgPromise.ITask<any>): Promise<User> {
+		return new Promise((resolve, reject) => {
+			const db = t || this.database;
+			return db.one(`SELECT id, password FROM ${this.tableName} WHERE email = $1`, email).then((user) => {
+				resolve(user);
+			}).catch((error) => { reject(new ResponseError('Invalid email or password', StatusCode.UNAUTHORIZED)); });
+		});
+	}
+	
+	public updatePassword(id: number, newPassword: string, t?: pgPromise.ITask<any>): Promise<User> {
+		return new Promise((resolve, reject) => {
+			const db = t || this.database;
+			return db.txIf((t_: pgPromise.ITask<any>) => {
+				return t_.one(`UPDATE ${this.tableName} SET password = $2 WHERE id = $1 RETURNING id`, [id, newPassword]).then((updatedUser) => {
+					return this.historyRepository.update(updatedUser.id, t_).then((updatedHistoryId) => {
+						resolve(updatedUser);
+					}).catch((error) => { reject(toResponseError(error)); });
+				}).catch((error) => { reject(toResponseError(error)); });
+			});
+		});
+	}
+}
