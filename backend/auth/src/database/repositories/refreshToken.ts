508f35fdb739a0a3413b6348ae589f74dbe4d095
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-02 18:27:54 +0100

    Refresh token repository

---
+++ backend/auth/src/database/repositories/refreshToken.ts
@@ -0,0 +1,50 @@
+import pgPromise from 'pg-promise';
+import { toResponseError } from '../errorParser';
+import { RefreshToken } from '../models/refreshToken';
+import { BaseRepository } from './base';
+import { BaseWithHistoryRepository } from './baseWithHistory';
+import { HistoryRepository } from './history';
+
+const tableName = 'refresh_tokens';
+export class RefreshTokenRepository extends BaseWithHistoryRepository<RefreshToken> {
+	constructor(database: pgPromise.IDatabase<any>, historyRepository: HistoryRepository) {
+		super(database, tableName, historyRepository);
+	}
+
+	public createTable(): Promise<null> {
+		return this.database.none(`
+			CREATE TABLE IF NOT EXISTS ${this.tableName}(
+				id INT PRIMARY KEY,
+				token CHAR (60)	NOT NULL UNIQUE,
+				users_id INT NOT NULL REFERENCES users (id),
+				histories_id INT NOT NULL REFERENCES histories (id)
+			)`
+		);
+	}
+
+	public insert(id: number, token: string, userId: number, t?: pgPromise.ITask<any>): Promise<number> {
+		return new Promise((resolve, reject) => {
+			const db = t || this.database;
+			return db.txIf((t_: pgPromise.ITask<any>) => {
+				return this.historyRepository.insert(t_).then((insertedHistoryId) => {
+					return t_.one(`INSERT INTO ${this.tableName}(id, token, users_id, histories_Id) VALUES ($1, $2, $3, $4) RETURNING id`, [id, token, userId, insertedHistoryId]).then((insertedToken) => {
+						resolve(insertedToken.id);
+					}).catch((error) => { reject(toResponseError(error)); });
+				}).catch((error) => { reject(toResponseError(error)); });
+			});
+		});
+	}
+
+	public getNextId(t?: pgPromise.ITask<any>): Promise<number> {
+		return new Promise((resolve, reject) => {
+			const db = t || this.database;
+			return db.oneOrNone(`SELECT MAX(id) AS max FROM ${this.tableName}`).then((result) => {
+				if (result.max === null) {
+					resolve(1);
+				} else {
+					resolve(result.max + 1);
+				}
+			}).catch((error) => { reject(toResponseError(error)); });
+		});
+	}
+}
