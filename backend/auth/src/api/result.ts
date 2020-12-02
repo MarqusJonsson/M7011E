6ea167723cf9845a94a212a73c9cb82bd7bc4bb8
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-02 18:25:25 +0100

    API request result definitions

---
+++ backend/auth/src/api/result.ts
@@ -0,0 +1,4 @@
+export class GetResult { constructor(public readonly data: any) {} }
+export class PostResult { constructor(public readonly data: any, public readonly location: any) {} }
+export class PutResult { constructor(public readonly data: any) {} }
+export class DeleteResult { constructor(public readonly data: any) {} }
