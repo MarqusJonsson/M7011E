1f068b0aa7927816e7062c4e46f5250a44c019ed
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-02 18:22:59 +0100

    Middleware for handling errors

---
+++ backend/auth/src/middleware/errors.ts
@@ -0,0 +1,15 @@
+import { ResponseError } from '../utils/error';
+
+export const errorHandler = (error: any, request: any, response: any, next: any) => {
+	if (error instanceof ResponseError) {
+		return response.status(error.statusCode).json({
+			success: false,
+			message: error.message
+		});
+	}
+	console.error(error.message);
+	return response.status(500).json({
+		success: false,
+		message: 'Unknown error!'
+	});
+}
