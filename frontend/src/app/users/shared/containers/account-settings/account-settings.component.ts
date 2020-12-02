3706c3bcd646e8d6d6866761d3e5e1a496320f8f
osksun <37369260+osksun@users.noreply.github.com>
2020-12-02 12:20:50 +0100

    Add image preview to account settings

--- frontend/src/app/users/shared/containers/account-settings/account-settings.component.ts
+++ frontend/src/app/users/shared/containers/account-settings/account-settings.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-account-settings',
@@ -5,9 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
-  constructor() { }
+
+fileData: File = null;
+previewUrl:any = null;
+fileUploadProgress: string = null;
+uploadedFilePath: string = null;
+constructor(private http: HttpClient) { }
+ 
+fileProgress(fileInput: any) {
+      this.fileData = <File>fileInput.target.files[0];
+      this.preview();
+}
+ 
+preview() {
+    // Show preview 
+    var mimeType = this.fileData.type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+ 
+    var reader = new FileReader();      
+    reader.readAsDataURL(this.fileData); 
+    reader.onload = (_event) => { 
+      this.previewUrl = reader.result; 
+    }
+}
+ 
+/*onSubmit() {
+    const formData = new FormData();
+      formData.append('file', this.fileData);
+      this.http.post('url/to/your/api', formData)
+        .subscribe(res => {
+          console.log(res);
+          this.uploadedFilePath = res.data.filePath;
+          alert('SUCCESS !!');
+        })
+}*/
+
   ngOnInit(): void {
   }
+
+
 
   
 
