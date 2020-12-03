c789a91b765bdba8bfa5c59810219ea43f859d73
osksun <37369260+osksun@users.noreply.github.com>
2020-12-03 15:19:24 +0100

    Add function to display values in GUI

--- frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page-children/visual-block/visual-block.component.ts
+++ frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page-children/visual-block/visual-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'prosumer-page-visual-block',
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visual-block.component.css']
 })
 export class VisualBlockComponent implements OnInit {
+  @ViewChild('production') production:ElementRef;
+  @ViewChild('consumption') consumption:ElementRef;
+  @ViewChild('netProduction') netProduction:ElementRef;
+  @ViewChild('temperature') temperature:ElementRef;
+  @ViewChild('windSpeed') windSpeed:ElementRef;
+  @ViewChild('battery') battery:ElementRef;
+  @ViewChild('batteryCapacity') batteryCapacity:ElementRef;
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  public setProduction(value: number) {
+    this.production.nativeElement.innerText = value;
+  }
+
+  public setConsumption(value: number) {
+    this.consumption.nativeElement.innerText = value;
+  }
+
+  public setNetProduction() {
+    let production = parseInt(this.production.nativeElement.innerText);
+    let consumption = parseInt(this.consumption.nativeElement.innerText);
+    let netProduction = production - consumption;
+    this.netProduction.nativeElement.innerText = netProduction;
+  }
+
+  public setTemperature(value: number) {
+    this.temperature.nativeElement.innerText = value + " C";
+  }
+
+  public setWindSpeed(value: number) {
+    this.windSpeed.nativeElement.innerText = value + " m/s";
+  }
+
+  public setBattery(value: number) {
+    this.battery.nativeElement.innerText = value + "/";
+  }
+
+  public setBatteryCapacity(value: number) {
+    this.batteryCapacity.nativeElement.innerText = value + " kwh";
   }
 
 }
