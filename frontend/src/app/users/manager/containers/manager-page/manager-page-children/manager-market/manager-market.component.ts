f0704a253f568b4ade10bdc20ecc6895aa92924b
osksun <37369260+osksun@users.noreply.github.com>
2020-12-03 20:30:13 +0100

    Add content to market component

--- frontend/src/app/users/manager/containers/manager-page/manager-page-children/manager-market/manager-market.component.ts
+++ frontend/src/app/users/manager/containers/manager-page/manager-page-children/manager-market/manager-market.component.ts
@@ -6,23 +6,36 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./manager-market.component.css']
 })
 export class ManagerMarketComponent implements OnInit {
-  @ViewChild('header') header:ElementRef; 
-  constructor(private hostElement: ElementRef) { }
+  @ViewChild('marketDemand') marketDemand:ElementRef;
+  @ViewChild('suggestPrice') suggestPrice:ElementRef; 
+  @ViewChild('electricityPrice') electricityPrice:ElementRef;
+  @ViewChild('currency') currency:ElementRef; 
+ 
 
+  constructor(private hostElement: ElementRef) { }
   ngOnInit(): void {
 
   }
 
   ngAfterViewInit() {
-    if(this.hostElement.nativeElement.attributes.getNamedItem("blockName") != null)
-      this.setHeaderValue(this.hostElement.nativeElement.attributes.getNamedItem("blockName").value);
-    else
-    this.setHeaderValue("Undefined");
- 
+
   }
 
-  public setHeaderValue(newValue: string) {
-    this.header.nativeElement.innerText = newValue;
+  public setmarkDemand(value: string) {
+    this.marketDemand.nativeElement.innerText = value;
   }
+
+  public setsuggestPrice(value: string) {
+    this.suggestPrice.nativeElement.innerText = value;
+  }
+
+  public setElectricityPrice(value: string) {
+    this.electricityPrice.nativeElement.innerText = value;
+  }
+
+  public setCurrency(value: string) {
+    this.currency.nativeElement.innerText = value;
+  }
+
 
 }
