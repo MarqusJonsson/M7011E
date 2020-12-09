4c50ff5a5975d5cb933ab89f7d8a8fb20c8acfc2
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-09 17:16:58 +0100

    Jwt interceptor

---
+++ frontend/src/app/api/interceptors/jwt.interceptor.ts
@@ -0,0 +1,69 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse, HttpEvent, HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
+import { catchError, filter, mergeMap, switchMap, take } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { StatusCode } from '../statusCode';
+import { AuthService } from '../../auth/services/auth.service';
+import { config } from '../../config';
+
+@Injectable()
+export default class JwtInterceptor implements HttpInterceptor {
+
+	private isRefreshing = false;
+	private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+	constructor(private authService: AuthService) {}
+
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		// Do not add authorization header to login and register request
+		if (request.url === config.URL_LOGIN || request.url === config.URL_REGISTER) {
+		return next.handle(request);
+		}
+		// Add authorization header with access token
+		request = this.addAccessToken(request, AuthService.getAccessToken());
+
+		return next.handle(request).pipe(
+		catchError((error) => {
+			if (error instanceof HttpErrorResponse) {
+			switch (error.status) {
+				case StatusCode.UNAUTHORIZED:
+				return this.handleUnauthorizedError(request, next);
+			}
+			}
+			return throwError(error);
+		})
+		);
+	}
+
+	private addAccessToken(request: HttpRequest<any>, accessToken: string) {
+		return request.clone({
+		setHeaders: {
+			authorization: 'Bearer ' + accessToken,
+			// 'Content-Type': 'application/json'
+		}
+		});
+	}
+
+	private handleUnauthorizedError(request: HttpRequest<any>, next: HttpHandler) {
+		if (!this.isRefreshing) {
+		this.isRefreshing = true;
+		this.refreshTokenSubject.next(null);
+		return this.authService.refreshAccessToken().pipe(
+			switchMap((response) => {
+			const accessToken = response.body.accessToken;
+			this.isRefreshing = false;
+			this.refreshTokenSubject.next(accessToken);
+			return next.handle(this.addAccessToken(request, accessToken));
+			}));
+		} else {
+		return this.refreshTokenSubject.pipe(
+			filter((response) => response != null),
+			take(1),
+			switchMap((accessToken) => {
+			AuthService.setAccessToken(accessToken);
+			return next.handle(request);
+			})
+		);
+		}
+	}
+}
