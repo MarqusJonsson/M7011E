d3c69a334efdd6b24ef4b526fc36addb7965aee2
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-09 17:18:31 +0100

    Jwt interceptor + Register/Login components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './users/shared/containers/login//login.component';
+import { LoginComponent } from './auth/containers/login//login.component';
 import { AlertComponent } from './alert/containers/alert/alert.component';
 import { ProsumerPageComponent } from './users/prosumer/containers/prosumer-page/prosumer-page.component';
 import { AccountSettingsComponent } from './users/shared/containers/account-settings/account-settings.component';
@@ -18,45 +17,58 @@ import { PlotlyComponent } from './users/shared/containers/plotly/plotly.compone
 import { RatioBlockComponent } from './users/prosumer/containers/prosumer-page/prosumer-page-children/ratio-block/ratio-block.component';
 PlotlyViaCDNModule.setPlotlyVersion('latest'); // can be `latest` or any version number (i.e.: '1.40.0')
 PlotlyViaCDNModule.setPlotlyBundle('basic'); // optional: can be null (for full) or 'basic', 'cartesian', 'geo', 'gl3d', 'gl2d', 'mapbox' or 'finance'
-import { HttpClientModule } from '@angular/common/http';
-import { RegistrationComponent } from './users/shared/containers/registration/registration.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RegisterComponent } from './auth/containers/register/register.component';
 import { ManagerPageComponent } from './users/manager/containers/manager-page/manager-page.component';
 import { ManagerNavBarComponent } from './users/manager/containers/manager-page/manager-page-children/manager-nav-bar/manager-nav-bar.component';
 import { ManagerMainBlockComponent } from './users/manager/containers/manager-page/manager-page-children/manager-main-block/manager-main-block.component';
 import { PowerPlantBlockComponent } from './users/manager/containers/manager-page/manager-page-children/power-plant-block/power-plant-block.component';
 import { SubBlockComponent } from './users/shared/containers/sub-block/sub-block.component';
 import { MarketComponent } from './users/manager/containers/manager-page/manager-page-children/market/market.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { AuthService } from './auth/services/auth.service';
+import JwtInterceptor from './api/interceptors/jwt.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    AlertComponent,
-    ProsumerPageComponent,
-    AccountSettingsComponent,
-    NavBarComponent,
-    VisualBlockComponent,
-    PageBlockComponent,
-    PlotlyComponent,
-    RatioBlockComponent,
-    RegistrationComponent,
-    ManagerPageComponent,
-    ManagerNavBarComponent,
-    ManagerMainBlockComponent,
-    PowerPlantBlockComponent,
-    SubBlockComponent,
-    MarketComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    PlotlyViaCDNModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		AlertComponent,
+		ProsumerPageComponent,
+		AccountSettingsComponent,
+		NavBarComponent,
+		VisualBlockComponent,
+		PageBlockComponent,
+		PlotlyComponent,
+		RatioBlockComponent,
+		RegisterComponent,
+		ManagerPageComponent,
+		ManagerNavBarComponent,
+		ManagerMainBlockComponent,
+		PowerPlantBlockComponent,
+		SubBlockComponent,
+		MarketComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		PlotlyViaCDNModule,
+		BrowserAnimationsModule,
+		MatSliderModule,
+		HttpClientModule,
+		JwtModule.forRoot({
+		config: {
+			tokenGetter: AuthService.getAccessToken,
+			allowedDomains: ['localhost:4200']
+		}
+		})
+	],
+	providers: [{
+		provide: HTTP_INTERCEPTORS,
+		useClass: JwtInterceptor,
+		multi: true
+	}],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
