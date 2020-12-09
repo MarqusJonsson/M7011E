b08b9a47b13026e1759de683b8e232974f8e1ce3
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-09 17:17:56 +0100

    Prosumer/manager guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './users/shared/containers/login/login.component';
+import { LoginComponent } from './auth/containers/login/login.component';
 import { ProsumerPageComponent } from './users/prosumer/containers/prosumer-page/prosumer-page.component';
 import { AccountSettingsComponent } from './users/shared/containers/account-settings/account-settings.component';
-import { RegistrationComponent } from './users/shared/containers/registration/registration.component';
+import { RegisterComponent } from './auth/containers/register/register.component';
 import { ManagerPageComponent } from './users/manager/containers/manager-page/manager-page.component';
+import { ProsumerGuard } from './auth/guards/prosumer.guard';
+import { ManagerGuard } from './auth/guards/manager.guard';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'login'},
-  {path:  'login', component: LoginComponent },
-  {path: 'registration', component: RegistrationComponent},
-  {path:  'prosumerPage', component: ProsumerPageComponent },
-  {path: 'settings' , component: AccountSettingsComponent},
-  {path: 'managerPage', component: ManagerPageComponent},
-
+	{path: '', pathMatch: 'full', redirectTo: '/login'},
+	{path: 'login', component: LoginComponent },
+	{path: 'register', component: RegisterComponent},
+	{path: 'prosumer-page', component: ProsumerPageComponent, canActivate: [ProsumerGuard]},
+	{path: 'manager-page', component: ManagerPageComponent, canActivate: [ManagerGuard]},
+	{path: 'settings' , component: AccountSettingsComponent},
+	{path: '**', redirectTo: '/login'}// Replace with 404 page
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
