1725bbecd243b426315d0a9b0a83b912bfac884b
osksun <37369260+osksun@users.noreply.github.com>
2020-12-13 13:44:56 +0100

    Add display of simulator values for prosumer

--- frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page-children/visual-block/visual-block.component.ts
+++ frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page-children/visual-block/visual-block.component.ts
@@ -1,60 +1,73 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
+import { geoDataContent, geoDataQuery } from 'src/app/api/models/geoDataContent';
+import { GraphqlService } from 'src/app/api/services/graphql.service';
+import { displayValuePrecision } from 'src/app/users/shared/pageConstants';
+import { Ws_to_kWh } from 'src/app/utils/electricity';
 @Component({
   selector: 'prosumer-page-visual-block',
   templateUrl: './visual-block.component.html',
   styleUrls: ['./visual-block.component.css']
 })
 export class VisualBlockComponent implements OnInit {
-  @ViewChild('production') production:ElementRef;
-  @ViewChild('consumption') consumption:ElementRef;
-  @ViewChild('netProduction') netProduction:ElementRef;
-  @ViewChild('temperature') temperature:ElementRef;
-  @ViewChild('windSpeed') windSpeed:ElementRef;
-  @ViewChild('battery') battery:ElementRef;
-  @ViewChild('batteryCapacity') batteryCapacity:ElementRef;
+	@ViewChild('production') production:ElementRef;
+	@ViewChild('consumption') consumption:ElementRef;
+	@ViewChild('netProduction') netProduction:ElementRef;
+	@ViewChild('temperature') temperature:ElementRef;
+	@ViewChild('windSpeed') windSpeed:ElementRef;
+	@ViewChild('battery') battery:ElementRef;
+	@ViewChild('batteryCapacity') batteryCapacity:ElementRef;
 
 
 
 
-  constructor() { }
+	constructor(private graphqlService: GraphqlService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  ngAfterViewInit() {
+	ngAfterViewInit() {
+		this.graphqlService.addSubscriberCallBack(this.onUpdate);
+	}
 
-  }
+	public setProduction(value: number) {
+		this.production.nativeElement.innerText = value + " kWh";
+	}
 
-  public setProduction(value: number) {
-    this.production.nativeElement.innerText = value;
-  }
+	public setConsumption(value: number) {
+		this.consumption.nativeElement.innerText =  Ws_to_kWh(value).toFixed(displayValuePrecision) + " kWh";
+	}
 
-  public setConsumption(value: number) {
-    this.consumption.nativeElement.innerText = value;
-  }
+	public setNetProduction() {
+		let production = parseFloat(this.production.nativeElement.innerText);
+		let consumption = parseFloat(this.consumption.nativeElement.innerText);
+		let netProduction = production - consumption;
+		this.netProduction.nativeElement.innerText = netProduction + " kWh";
+	}
 
-  public setNetProduction() {
-    let production = parseInt(this.production.nativeElement.innerText);
-    let consumption = parseInt(this.consumption.nativeElement.innerText);
-    let netProduction = production - consumption;
-    this.netProduction.nativeElement.innerText = netProduction;
-  }
+	public setTemperature(value: number) {
+		this.temperature.nativeElement.innerText =value.toFixed(displayValuePrecision) + " C";
+	}
 
-  public setTemperature(value: number) {
-    this.temperature.nativeElement.innerText = value + " C";
-  }
+	public setWindSpeed(value: number) {
+		this.windSpeed.nativeElement.innerText = value.toFixed(displayValuePrecision) + " m/s";
+	}
 
-  public setWindSpeed(value: number) {
-    this.windSpeed.nativeElement.innerText = value + " m/s";
-  }
+	public setBattery(value: number) {
+		this.battery.nativeElement.innerText =  Ws_to_kWh(value).toFixed(displayValuePrecision) + "/";
+	}
 
-  public setBattery(value: number) {
-    this.battery.nativeElement.innerText = value + "/";
-  }
+	public setBatteryCapacity(value: number) {
+		this.batteryCapacity.nativeElement.innerText = Ws_to_kWh(value).toFixed(displayValuePrecision) + " kWh";
+	}
 
-  public setBatteryCapacity(value: number) {
-    this.batteryCapacity.nativeElement.innerText = value + " kwh";
-  }
+	public onUpdate = (data: any) => {
+		this.setProduction(data.house.electricityProduction);
+		this.setConsumption(data.house.electricityConsumption);
+		this.setNetProduction();
+		this.setTemperature(data.house.geoData.temperature);
+		this.setWindSpeed(data.house.geoData.windSpeed);
+		this.setBattery(data.house.battery.buffer);
+		this.setBatteryCapacity(data.house.battery.capacity);
+	}
 
 }
