14277aeb204217126465a6b5ed3367fb6273cfe0
osksun <37369260+osksun@users.noreply.github.com>
2020-12-13 13:44:22 +0100

    Switch to using callbacks for queries

--- frontend/src/app/api/services/graphql.service.ts
+++ frontend/src/app/api/services/graphql.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import {Apollo} from 'apollo-angular';
 import gql from 'graphql-tag';
-
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class GraphqlService {
-error: any;
-  constructor(private apollo: Apollo) { }
-  ngOnInit(){
-    this.apollo.watchQuery({
-      query: gql `
-        {
-          graphql(currency: "USD) {
 
-          }
-        }
-      `,
-    }).valueChanges.subscribe(result => {
-      this.error = result.error;
-    })
-  }
+export class GraphqlService {
+	subscriberCallBacks: ((data: any) => void)[] = [];
+	constructor(private apollo: Apollo) { }
+	ngOnInit(){}
+	query(queryContent: any) {
+		return this.apollo.query({
+			query: gql`${queryContent}` 
+		}).pipe(
+			map((response: any) => {
+				this.subscriberCallBacks.forEach((callback) => {
+					callback(response.data);
+				})
+				return response.data;
+			})
+		);
+	}
+
+	addSubscriberCallBack(callback: (data:any) => void) {
+		this.subscriberCallBacks.push(callback);
+	}
 }
