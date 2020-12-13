31eef838e8232e06458ab61df9bc2a61040c8779
osksun <37369260+osksun@users.noreply.github.com>
2020-12-13 14:47:58 +0100

    Add display functions for graphql requests

--- frontend/src/app/users/manager/containers/manager-page/manager-page-children/power-plant-block/power-plant-block.component.ts
+++ frontend/src/app/users/manager/containers/manager-page/manager-page-children/power-plant-block/power-plant-block.component.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GraphqlService } from 'src/app/api/services/graphql.service';
+import { displayValuePrecision } from 'src/app/users/shared/pageConstants';
+import { Ws_to_kWh } from 'src/app/utils/electricity';
 
 @Component({
   selector: 'power-plant-block',
@@ -11,37 +14,56 @@ export class PowerPlantBlockComponent implements OnInit {
   @ViewChild('netProduction') netProduction:ElementRef;
   @ViewChild('battery') battery:ElementRef;
   @ViewChild('batteryCapacity') batteryCapacity:ElementRef;
-  constructor(private hostElement: ElementRef) { }
+  @ViewChild('status') status:ElementRef;
+
+  constructor(private hostElement: ElementRef, private graphqlService: GraphqlService) { }
 
   ngOnInit(): void {
 
   }
 
   ngAfterViewInit() {
+    this.graphqlService.addSubscriberCallBack(this.onUpdate);
 
   }
 
   public setProduction(value: number) {
-    this.production.nativeElement.innerText = value;
+    this.production.nativeElement.innerText =  Ws_to_kWh(value).toFixed(displayValuePrecision) + " kWh";
   }
 
   public setConsumption(value: number) {
-    this.consumption.nativeElement.innerText = value;
+    this.consumption.nativeElement.innerText =  Ws_to_kWh(value).toFixed(displayValuePrecision) + " kWh";
   }
 
   public setNetProduction() {
-    let production = parseInt(this.production.nativeElement.innerText);
-    let consumption = parseInt(this.consumption.nativeElement.innerText);
+    let production = parseFloat(this.production.nativeElement.innerText);
+    let consumption = parseFloat(this.consumption.nativeElement.innerText);
     let netProduction = production - consumption;
-    this.netProduction.nativeElement.innerText = netProduction;
+    this.netProduction.nativeElement.innerText = netProduction.toFixed(displayValuePrecision) + " kWh";
   }
 
   public setBattery(value: number) {
-    this.battery.nativeElement.innerText = value + "/";
+    this.battery.nativeElement.innerText =  Ws_to_kWh(value).toFixed(displayValuePrecision) + "/";
   }
 
   public setBatteryCapacity(value: number) {
-    this.batteryCapacity.nativeElement.innerText = value + " kwh";
+    this.batteryCapacity.nativeElement.innerText =  Ws_to_kWh(value).toFixed(displayValuePrecision) + " kWh";
+  }
+
+  public setStatus(value: boolean) {
+    if(value == false)
+    this.status.nativeElement.innerText = "Online";
+    else
+    this.status.nativeElement.innerText = "Blackout"; 
+  }
+
+  public onUpdate = (data: any) => {
+    this.setBattery(data.powerPlant.battery.buffer);
+    this.setBatteryCapacity(data.powerPlant.battery.capacity);
+    this.setProduction(data.powerPlant.electricityProduction);
+    this.setConsumption(data.powerPlant.electricityConsumption)
+    this.setNetProduction();
+    this.setStatus(data.powerPlant.hasBlackout);
   }
 
 }
