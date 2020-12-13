8c9afad102903f1f13e6190cf86bc4b898b82173
osksun <37369260+osksun@users.noreply.github.com>
2020-12-13 17:28:23 +0100

    Add display of graphql queries

--- frontend/src/app/users/manager/containers/manager-page/manager-page-children/manager-market/manager-market.component.ts
+++ frontend/src/app/users/manager/containers/manager-page/manager-page-children/manager-market/manager-market.component.ts
@@ -1,41 +1,55 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GraphqlService } from 'src/app/api/services/graphql.service';
+import { displayValuePrecision } from 'src/app/users/shared/pageConstants';
+import { Ws_to_kWh, kWh_to_Ws } from 'src/app/utils/electricity';
 
 @Component({
-  selector: 'manager-market-block',
-  templateUrl: './manager-market.component.html',
-  styleUrls: ['./manager-market.component.css']
+	selector: 'manager-market-block',
+	templateUrl: './manager-market.component.html',
+	styleUrls: ['./manager-market.component.css']
 })
 export class ManagerMarketComponent implements OnInit {
-  @ViewChild('marketDemand') marketDemand:ElementRef;
-  @ViewChild('suggestPrice') suggestPrice:ElementRef; 
-  @ViewChild('electricityPrice') electricityPrice:ElementRef;
-  @ViewChild('currency') currency:ElementRef; 
+	@ViewChild('marketDemand') marketDemand:ElementRef;
+	@ViewChild('suggestBuyPrice') suggestBuyPrice:ElementRef;
+	@ViewChild('suggestSellPrice') suggestSellPrice:ElementRef;
+	@ViewChild('electricityPrice') electricityPrice:ElementRef;
+	@ViewChild('currency') currency:ElementRef; 
  
+	constructor(private hostElement: ElementRef, private graphqlService: GraphqlService) { }
+		ngOnInit(): void {
 
-  constructor(private hostElement: ElementRef) { }
-  ngOnInit(): void {
+	}
 
-  }
+	ngAfterViewInit() {
+		this.graphqlService.addSubscriberCallBack(this.onUpdate);
+	}
+	public setMarketDemand(value: number) {
+		this.marketDemand.nativeElement.innerText = kWh_to_Ws(value).toFixed(displayValuePrecision) + " kwh/currency";
+	}
 
-  ngAfterViewInit() {
+	public setSuggestBuyPrice(value: number) {
+		this.suggestBuyPrice.nativeElement.innerText = kWh_to_Ws(value).toFixed(displayValuePrecision) + " kWh/currency";
+	}
 
-  }
+	public setSuggestSellPrice(value: number) {
+		this.suggestSellPrice.nativeElement.innerText = kWh_to_Ws(value).toFixed(displayValuePrecision) + " kWh/currency";
+	}
 
-  public setmarkDemand(value: string) {
-    this.marketDemand.nativeElement.innerText = value;
-  }
+	public setElectricityPrice(value: number) {
+		this.electricityPrice.nativeElement.innerText = kWh_to_Ws(value).toFixed(displayValuePrecision) + " kWh/currency";
+	}
 
-  public setsuggestPrice(value: string) {
-    this.suggestPrice.nativeElement.innerText = value;
-  }
+	public setCurrency(value: number) {
+		this.currency.nativeElement.innerText = value.toFixed(displayValuePrecision);
+	}
 
-  public setElectricityPrice(value: string) {
-    this.electricityPrice.nativeElement.innerText = value;
-  }
+	public onUpdate = (data: any) => {
+		this.setCurrency(data.manager.currency);
+		this.setElectricityPrice(data.manager.powerPlant.electricityBuyPrice);
+		this.setSuggestBuyPrice(data.manager.powerPlant.modelledElectricityBuyPrice);
+		this.setSuggestSellPrice(data.manager.powerPlant.modelledElectricitySellPrice);
 
-  public setCurrency(value: string) {
-    this.currency.nativeElement.innerText = value;
-  }
+	}
 
 
 }
