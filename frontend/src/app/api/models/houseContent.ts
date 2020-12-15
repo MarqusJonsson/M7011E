840a55a9241cc3f7a46e1efec362b338a1d77122
osksun <37369260+osksun@users.noreply.github.com>
2020-12-15 17:38:57 +0100

    Add additional queries

--- frontend/src/app/api/models/houseContent.ts
+++ frontend/src/app/api/models/houseContent.ts
@@ -4,4 +4,6 @@ import { geoDataContent } from './geoDataContent';
 import { prosumerPowerPlantContent } from './powerPlantContent';
 
 export const houseContent = "house {electricityConsumption electricityProduction batteryToPowerPlantRatio hasBlackout " + batteryContent + geoDataContent + generatorsContent + prosumerPowerPlantContent +"}";
+export const houseContentWithoutPowerPlant = "house {electricityConsumption electricityProduction batteryToPowerPlantRatio hasBlackout " + batteryContent + geoDataContent + generatorsContent +"}";
+
 export const houseQuery = `query house {${houseContent}}`
--- frontend/src/app/api/models/prosumerContent.ts
+++ frontend/src/app/api/models/prosumerContent.ts
@@ -1,4 +1,6 @@
-import { houseContent } from "./houseContent";
+import { houseContent, houseContentWithoutPowerPlant } from "./houseContent";
 
 export const prosumerContent = `prosumer {currency ${houseContent}}`;
 export const prosumerQuery = `query prosumer {${prosumerContent}}`;
+export const prosumerContentById = `prosumer (id: $id) {${houseContentWithoutPowerPlant}}`
+export const prosumerQueryById = `query prosumerById ($id: ID!) {${prosumerContentById}}`
