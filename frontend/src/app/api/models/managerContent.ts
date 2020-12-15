06c6cb5f852166e1e6823f8a785180b22e41ccc2
osksun <37369260+osksun@users.noreply.github.com>
2020-12-15 11:08:38 +0100

    Adjust manager query content

--- frontend/src/app/api/models/managerContent.ts
+++ frontend/src/app/api/models/managerContent.ts
@@ -1,5 +1,5 @@
 import { houseContent } from "./houseContent";
 import { powerPlantContent } from "./powerPlantContent";
 
-export const managerContent = `manager {currency ${powerPlantContent} prosumers {id house{battery {buffer capacity}}}}`;
+export const managerContent = `manager {currency ${powerPlantContent} prosumers {id}}`;
 export const managerQuery = `query manager {${managerContent}}`;
