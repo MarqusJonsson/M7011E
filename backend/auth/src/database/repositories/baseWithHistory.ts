3460470e84f814b687791eed77b1431790df0d3f
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-15 18:01:34 +0100

    Function to delete old entries in database

--- backend/auth/src/database/repositories/baseWithHistory.ts
+++ backend/auth/src/database/repositories/baseWithHistory.ts
@@ -25,4 +25,18 @@ export abstract class BaseWithHistoryRepository<T> extends BaseRepository<T> {
 			}).catch((error) => { reject(toResponseError(error)); });
 		});
 	}
+
+	public deleteOld(maxAgeSeconds: number, t?: pgPromise.ITask<any>) {
+		return new Promise<number>((resolve, reject) => {
+			const db = t || this.database;
+			return db.txIf((t_: pgPromise.ITask<any>) => {
+				return db.oneOrNone(`
+					DELETE FROM ${this.historyRepository.tableName} AS h
+					USING ${this.tableName} AS t
+					WHERE t.histories_id = h.id AND
+					AGE(now(), h.created_at) > INTERVAL '$1 seconds'`, maxAgeSeconds
+				).catch((error) => { reject(toResponseError(error)); });
+			}).catch((error) => { reject(toResponseError(error)); });
+		});
+	}
 }
