cb1477d20ef554ae100a7b99ff387cd9caef499e
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-15 11:54:18 +0100

    Allow managers to query prosumer data by prosumer id

--- backend/simulator/src/api/schemas/prosumer/queries.ts
+++ backend/simulator/src/api/schemas/prosumer/queries.ts
@@ -6,16 +6,21 @@ import { Prosumer } from '../../../users/prosumer';
 import { GraphQLError } from 'graphql/error/GraphQLError';
 import { Manager } from '../../../users/manager';
 import { Identifier } from '../../../identifiable';
+import { GraphQLID } from 'graphql';
 
 const prosumer = {
 	type: ProsumerType,
+	args: { id: { type: GraphQLID }},
 	description: `Returns a ${typeName} with matching id.`,
 	resolve(parent: any, args: any, context: GraphQLContext) {
 		switch (context.user.type) {
 			case Prosumer.name:
 				return prosumerResolver.findByUser(context.simulator, context.user);
 			case Manager.name:
+				if (args === undefined) {
 					return prosumerResolver.findByUser(context.simulator, new Identifier(Prosumer.name, parent.id));
+				}
+				return prosumerResolver.findByUser(context.simulator, new Identifier(Prosumer.name, args.id));
 			default:
 				throw new GraphQLError(GraphQLErrorName.INVALID_USER_TYPE);
 		}
