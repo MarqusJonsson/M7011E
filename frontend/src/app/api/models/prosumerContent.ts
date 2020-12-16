c536ebd6607e9f6dc925772ff813d7ae0705e242
osksun <37369260+osksun@users.noreply.github.com>
2020-12-16 21:09:30 +0100

    Add block status prosumers

--- frontend/src/app/api/models/prosumerContent.ts
+++ frontend/src/app/api/models/prosumerContent.ts
@@ -2,5 +2,5 @@ import { houseContent, houseContentWithoutPowerPlant } from "./houseContent";
 
 export const prosumerContent = `prosumer {currency isBlocked ${houseContent}}`;
 export const prosumerQuery = `query prosumer {${prosumerContent}}`;
-export const prosumerContentById = `prosumer (id: $id) {${houseContentWithoutPowerPlant}}`
+export const prosumerContentById = `prosumer (id: $id) { isBlocked ${houseContentWithoutPowerPlant}}`
 export const prosumerQueryById = `query prosumerById ($id: ID!) {${prosumerContentById}}`
