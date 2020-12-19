68951c74805f25f262dd82866a5aa63c2372f07d
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-12-19 17:15:24 +0100

    Logout button component

---
+++ frontend/src/app/auth/containers/logout/logout.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GraphqlService } from 'src/app/api/services/graphql.service';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+	selector: 'app-logout-button',
+	templateUrl: './logout.component.html',
+	styleUrls: [`./logout.component.css`]
+})
+export class LogoutComponent implements OnInit {
+	constructor(private authService: AuthService, private router: Router, private graphQLService: GraphqlService){}
+
+	ngOnInit(): void {
+	}
+
+	logout() {
+		this.graphQLService.stopQueryInterval();
+		this.authService.logout().subscribe(() => {
+			this.router.navigateByUrl('/login'); // Redirect when ready
+		});
+		return false; // return false so user do not get redirected from clicking anchor tag
+	}
+}
