8d23b8d028fbe3086cf17abb336f8fc38c95cfeb
osksun <37369260+osksun@users.noreply.github.com>
2020-12-23 10:52:10 +0100

    Add market sell price in ratio block

--- frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page-children/prosumer-currency-block/prosumer-currency-block.component.ts
+++ frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page-children/prosumer-currency-block/prosumer-currency-block.component.ts
@@ -9,7 +9,9 @@ import { displayValuePrecision } from 'src/app/users/shared/pageConstants';
 })
 export class ProsumerCurrencyBlockComponent implements OnInit {
 	@ViewChild('currency') currency:ElementRef;
-	@ViewChild('marketPrice') marketPrice:ElementRef;
+	@ViewChild('marketBuyPrice') marketBuyPrice:ElementRef;
+	@ViewChild('marketSellPrice') marketSellPrice:ElementRef;
+
 	constructor(private graphqlService: GraphqlService) { }
 
 	ngOnInit(): void {
@@ -23,13 +25,19 @@ export class ProsumerCurrencyBlockComponent implements OnInit {
 		this.currency.nativeElement.innerText = value.toFixed(displayValuePrecision);
 	}
 
-	public setMarketPrice(value: number) {
-		this.marketPrice.nativeElement.innerText = (value * 3600000).toFixed(displayValuePrecision) + " currency/kWh";
+	public setMarketBuyPrice(value: number) {
+		this.marketBuyPrice.nativeElement.innerText = (value * 3600000).toFixed(displayValuePrecision) + " currency/kWh";
+	}
+
+	public setMarketSellPrice(value: number) {
+		this.marketSellPrice.nativeElement.innerText = (value * 3600000).toFixed(displayValuePrecision) + " currency/kWh";
 	}
 
 	public onUpdate = (data: any) => {
 		this.setCurrency(data.prosumer.currency);
-		this.setMarketPrice(data.prosumer.house.powerPlant.electricityBuyPrice);
+		this.setMarketBuyPrice(data.prosumer.house.powerPlant.electricityBuyPrice);
+		this.setMarketSellPrice(data.prosumer.house.powerPlant.electricitySellPrice);
+
 	}
 
 }
