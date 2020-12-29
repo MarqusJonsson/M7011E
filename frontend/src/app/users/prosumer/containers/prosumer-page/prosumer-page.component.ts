7afc83e156e52bf4e63a38db90792ce0472d7e68
Markus Jonsson <markusjonsson1997@gmail.com>
2020-12-29 11:55:32 +0100

    Cleanup code

--- frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page.component.ts
+++ frontend/src/app/users/prosumer/containers/prosumer-page/prosumer-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { geoDataContent, geoDataQuery } from 'src/app/api/models/geoDataContent';
-import { houseContent, houseQuery } from 'src/app/api/models/houseContent';
 import { prosumerQuery } from 'src/app/api/models/prosumerContent';
 import { GraphqlService } from '../../../../api/services/graphql.service';
 @Component({
@@ -13,9 +11,6 @@ export class ProsumerPageComponent implements OnInit {
 	constructor(private graphqlService: GraphqlService) { }
 
 	ngOnInit(): void {
-	}
-
-	ngAfterViewInit() {
 		this.graphqlService.startQueryInterval(prosumerQuery, 1000);
 	}
 }
