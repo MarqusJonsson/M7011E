4da405cd1951ba024884b0b2ace8d0f94c35f9fc
osksun <37369260+osksun@users.noreply.github.com>
2020-12-31 14:54:14 +0100

    Add alertservice to deleteprosumerservice

--- frontend/src/app/users/shared/services/delete-prosumer.service.ts
+++ frontend/src/app/users/shared/services/delete-prosumer.service.ts
@@ -3,22 +3,30 @@ import { config } from 'src/app/config';
 import { HttpClient } from '@angular/common/http';
 import { deleteProsumerMutation } from 'src/app/api/models/mutations/prosumerMutations';
 import { GraphqlService } from 'src/app/api/services/graphql.service';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { of} from 'rxjs';
+import { AlertService } from 'src/app/alert/services/alert.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class DeleteProsumerService {
 
-	constructor(private graphqlService: GraphqlService, private http: HttpClient) { }
+	constructor(private graphqlService: GraphqlService, private http: HttpClient, private alertService: AlertService) { }
 
 	public deleteProsumer(id: number) {
-		return this.http.delete<any>(config.URL_DELETE_USER + `${id}`).pipe(map( (response) => {
-			if (response.success === false) {
-				//TODO Add error handling
-				throw new Error("TODO ERROR DELETE PROSUMER");
-			}
-			return this.graphqlService.mutate(deleteProsumerMutation, {id: id}).subscribe();
-		}));
+		return this.http.delete<any>(config.URL_DELETE_USER + `${id}`).pipe(
+			map( (response) => {
+				if (response.success === false) {
+					console.log(response.success, "in IF")
+					//throw new Error("Delete user failed");
+				}
+				//this.graphqlService.mutate(deleteProsumerMutation, {id: id}).subscribe();
+				return true;
+			}), catchError((error) => {
+				console.log(error, "in catcherror")
+				this.alertService.error("Failed to delete user");
+				return of(false);
+			}));
 	}
 }
