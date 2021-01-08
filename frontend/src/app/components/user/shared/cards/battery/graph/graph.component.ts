610597c964989a88195578f574c21ed1df5ddf86
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2021-01-08 15:50:15 +0100

    Move graph component to battery

---
+++ frontend/src/app/components/user/shared/cards/battery/graph/graph.component.ts
@@ -0,0 +1,98 @@
+import { Component, ElementRef } from '@angular/core';
+import * as Plotly from 'plotly.js';
+
+@Component({
+	selector: 'app-data-graph',
+	templateUrl: './graph.component.html',
+	styleUrls: ['./graph.component.css']
+})
+export class GraphComponent {
+
+	public id: string;
+	private data: Partial<Plotly.PlotData>[];
+	private layout: Partial<Plotly.Layout>;
+
+	constructor(private hostElement: ElementRef<HTMLElement>) {
+		this.id = this.hostElement.nativeElement.id + '-content';
+	}
+
+	public createPlot(hexColor: string, variables: { name?: string, lineColor: string }[]) {
+		this.data = [];
+		variables.forEach((variable) => {
+			this.data.push({
+				name: variable.name || '',
+				x: [] as Plotly.Datum[] | Plotly.Datum[][] | Plotly.TypedArray,
+				y: [] as Plotly.Datum[] | Plotly.Datum[][] | Plotly.TypedArray,
+				type: 'scatter',
+				mode: 'lines',
+				line: {
+					color: variable.lineColor,
+					shape: 'linear',
+					width: 3
+				}
+			});
+		});
+		this.layout = this.createLayout(hexColor);
+		Plotly.newPlot(this.id, this.data, this.layout);
+	}
+
+	public appendToPlot(x: any[][], y: any[][]) {
+		const indices = [...Array(x.length).keys()];
+		const graphDiv = this.hostElement.nativeElement.children[0] as any;
+		Plotly.extendTraces(this.id, { x: x, y: y }, indices);
+		// Remove every second data sample if trace contains more than 2500 points to prevent eventual performance issues
+		for (const trace of graphDiv.data) {
+			if (trace.x.length > 250) {
+				trace.x = trace.x.filter((_: any, index: number) => index % 2 === 0);
+				trace.y = trace.y.filter((_: any, index: number) => index % 2 === 0);
+			}
+		}
+	}
+
+	private createLayout(hexColor: string): Partial<Plotly.Layout> {
+		return {
+			paper_bgcolor: '#00000000',
+			plot_bgcolor: '#00000000',
+			height: 270,
+			legend: {
+				x: 1,
+				y: 0,
+				xanchor: 'right',
+				bgcolor: `${hexColor}6f`
+			},
+			font: {
+				family: 'sans-serif',
+				size: 12,
+				color: hexColor
+			},
+			margin: {
+				l: 10,
+				r: 10,
+				b: 40,
+				t: 20
+			},
+			grid: {
+				rows: 1,
+				columns: 1,
+				pattern: 'independent'
+			},
+			xaxis : {
+				tickangle: 0,
+				showticklabels: true,
+				showgrid: true,
+				gridcolor: `${hexColor}6f`,
+				zerolinecolor: `${hexColor}6f`
+			},
+			yaxis : {
+				showticklabels: true,
+				ticksuffix: ' %',
+				tickformat: `.3f`,
+				range: [0, 100],
+				position: 1,
+				showgrid: true,
+				gridcolor: `${hexColor}6f`,
+				zerolinecolor: `${hexColor}6f`
+			}
+		};
+	}
+}
