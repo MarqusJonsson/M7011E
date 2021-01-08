fb8e03c3224cc715e2aa04e7d363e4d9f84c1a78
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2021-01-08 15:47:00 +0100

    Single fetch callbacks

--- frontend/src/app/services/graphql/graphql.service.ts
+++ frontend/src/app/services/graphql/graphql.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Apollo} from 'apollo-angular';
+import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
-import { of } from 'rxjs';
+import { EMPTY } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 @Injectable({
 	providedIn: 'root'
 })
 
 export class GraphqlService {
-	subscriberCallBacks: ((data: any) => void)[] = [];
+	private subscriberCallbacks: ((data: any) => void)[] = [];
+	private newSubscriberCallbacks: ((data: any) => void)[] = [];
+	private singleFetchCallbacks: ((data: any) => void)[] = [];
+	private newSingleFetchCallbacks: ((data: any) => void)[] = [];
 	private queryInterval: NodeJS.Timeout;
 	constructor(private apollo: Apollo) { }
 
-	query(query: any, variables?: any) {
+	public query(query: any, variables?: any) {
 		return this.apollo.query({
 			query: gql`${query}`,
 			variables: variables,
@@ -22,12 +25,12 @@ export class GraphqlService {
 				return response.data;
 			}),
 			catchError((error) => {
-				return of(undefined);
+				return EMPTY;
 			})
 		);
 	}
 
-	mutate(mutation: any, variables?: any) {
+	public mutate(mutation: any, variables?: any) {
 		if (variables !== undefined) {
 			return this.apollo.mutate({
 				mutation: gql`${mutation}`,
@@ -45,37 +48,55 @@ export class GraphqlService {
 		}
 	}
 
-	startQueryInterval(query: any, ms: number) {
+	public startQueryInterval(query: any, ms: number) {
 		this.stopQueryInterval();
+		this.queryAndNotifySubscribers(query).subscribe();
 		this.queryInterval = setInterval(() => {
+			// Add new callbacs
+			if (this.newSubscriberCallbacks.length > 0) {
+				this.subscriberCallbacks.push(...this.newSubscriberCallbacks);
+				this.newSubscriberCallbacks.length = 0;
+			}
+			if (this.newSingleFetchCallbacks.length > 0) {
+				this.singleFetchCallbacks.push(...this.singleFetchCallbacks);
+				this.newSingleFetchCallbacks.length = 0;
+			}
 			this.queryAndNotifySubscribers(query).subscribe();
 		}, ms);
 	}
 
-	stopQueryInterval() {
+	public stopQueryInterval() {
 		if (this.queryInterval !== undefined) {
 			clearInterval(this.queryInterval);
 		}
 	}
 
-	queryAndNotifySubscribers(query: any) {
+	private queryAndNotifySubscribers(query: any) {
 		return this.query(query).pipe(
 			map((data: any) => {
 				if (data !== undefined) {
-					this.subscriberCallBacks.forEach((callback) => {
+					this.subscriberCallbacks.forEach((callback) => {
 						callback(data);
 					});
+					this.singleFetchCallbacks.forEach((callback) => {
+						callback(data);
+					})
+					this.singleFetchCallbacks.length = 0;
 				}
 				return data;
 			})
 		);
 	}
 
-	addSubscriberCallback(callback: (data: any) => void) {
-		this.subscriberCallBacks.push(callback);
+	public addSingleFetchCallback(callback: (data: any) => void) {
+		this.singleFetchCallbacks.push(callback);
 	}
 
-	removeAllSubscriberCallbacks() {
-		this.subscriberCallBacks.length = 0;
+	public addSubscriberCallback(callback: (data: any) => void) {
+		this.subscriberCallbacks.push(callback);
+	}
+
+	public removeAllSubscriberCallbacks() {
+		this.subscriberCallbacks.length = 0;
 	}
 }
