be81c0e760a3a5577efcecfbc8b86c7e3fd9c78b
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2021-01-08 16:16:01 +0100

    Usage of new visuals

--- frontend/src/app/components/user/manager-page/manager-page.component.ts
+++ frontend/src/app/components/user/manager-page/manager-page.component.ts
@@ -1,17 +1,35 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { managerQuery } from 'src/app/models/graphql/manager';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { managerQuery, ManagerQueryData } from 'src/app/models/graphql/manager';
 import { GraphqlService } from 'src/app/services/graphql/graphql.service';
+import { BatteryCardComponent } from '../shared/cards/battery/battery-card.component';
+import { GeoDataCardComponent } from '../shared/cards/geo-data/geo-data-card.component';
+import { PowerPlantCardComponent } from '../shared/cards/power-plant/power-plant-card.component';
+import { UserCardComponent } from '../shared/cards/user/user-card.component';
 
 @Component({
-	selector: 'manager-page',
+	selector: 'app-manager-page',
 	templateUrl: './manager-page.component.html',
 	styleUrls: ['./manager-page.component.css']
 })
 export class ManagerPageComponent implements AfterViewInit {
+	@ViewChild('batteryCard') batteryCard: BatteryCardComponent;
+	@ViewChild('geoDataCard') geoDataCard: GeoDataCardComponent;
+	@ViewChild('userCard') userCard: UserCardComponent;
+	@ViewChild('powerPlantCard') powerPlantCard: PowerPlantCardComponent;
 
 	constructor(private graphqlService: GraphqlService) { }
 
 	ngAfterViewInit() {
+		this.graphqlService.addSubscriberCallback((data: ManagerQueryData) => {
+			const { manager } = data;
+			this.batteryCard.update(manager.powerPlant.battery);
+			this.geoDataCard.update(manager.powerPlant.geoData);
+			this.userCard.update(manager.currency);
+			this.powerPlantCard.update(manager.powerPlant);
+		});
+		this.graphqlService.addSingleFetchCallback((data: ManagerQueryData) => {
+			this.powerPlantCard.setProductionOutputRatioSliderValue(data.manager.powerPlant.productionOutputRatio);
+		});
 		this.graphqlService.startQueryInterval(managerQuery, 1000);
 	}
 }
